3a9ce90fe31bee2e0c574ef16aa5b61e0a07ca09
KevinNyiringango <kevin.nyiringango@moyotech.solutions>
2025-07-01 14:16:37 +0200

    enable vercel

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Local development
         "https://safecell.netlify.app"  # Your deployed Netlify app
+        "https://safecell-front-end.vercel.app/"
     ],  
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE"],
